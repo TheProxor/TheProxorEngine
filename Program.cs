5933fdba739f0f07081c5f89cd5f15cb8736b9ac
TheProxor <proxortv@gmail.com>
2019-12-06 07:54:41 +0700

    Some inital things

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheProxorEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
